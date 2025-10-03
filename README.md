d38a9cc28a02bad45ed861bbd7275f9733b00f62
RubenSDW <145385591+RubenSDW@users.noreply.github.com>
2025-10-03 23:59:18 +0200

    Upload Project

---
+++ README.md
@@ -0,0 +1,26 @@
+# FUT WL Counter
+
+![sample](/readme/20_0.png)
+
+## Win / Lose counter for Fifa 23 Streamers Weekend League
+
+### How to add
+
+- Go to your OBS and add the web site (https://inca3.github.io/fut-wl-counter) as a **'Browser Source'** and use **Width: 276**, **Height: 146** for the best **display ratio**.
+- After adding browser source, copy the same url and go to docks, **Custom Browser Bocks**, and add this url as a dock.
+- The counter will pop up on your OBS after this stage. Simply drag this popup and place it where ever you like.
+- Now, you can update your score directly from that panel.
+
+![howtoadd](/readme/howtouse.gif)
+
+### How it works
+
+You can now control the scoreboard from that panel.
+
+- Left click on numbers will increase the number
+- Right click on number will decrease the number
+- Clicking in the middle (FUT Champions logo) will reset the scoreboard.
+
+![howtoadd](/readme/usage.gif)
+
+Any feedback is appreciated.
---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [wins, setWins] = useState(localStorage.getItem('wins') || 0);
+  const [loses, setLoses] = useState(localStorage.getItem('loses') || 0);
+  const [rivals, setRivals] = useState(localStorage.getItem('rivals') || 999);
+
+  const addWin = () => {
+    if (wins >= 20) return;
+    setWins((wins) => parseInt(wins) + 1);
+    localStorage.setItem('wins', parseInt(wins) + 1);
+  };
+  const removeWin = (e) => {
+    e.preventDefault();
+    if (wins <= 0) return;
+    setWins((wins) => parseInt(wins) - 1);
+    localStorage.setItem('wins', parseInt(wins) - 1);
+  };
+  const addLose = () => {
+    if (loses >= 20) return;
+    setLoses((loses) => parseInt(loses) + 1);
+    localStorage.setItem('loses', parseInt(loses) + 1);
+  };
+  const removeLose = (e) => {
+    e.preventDefault();
+    if (loses <= 0) return;
+    setLoses((loses) => parseInt(loses) - 1);
+    localStorage.setItem('loses', parseInt(loses) - 1);
+  };
+  const reset = () => {
+    setWins(0);
+    setLoses(0);
+    localStorage.setItem('loses', 0);
+    localStorage.setItem('wins', 0);
+  };
+  const changeRivals = (e) => {
+    // if (e.target.value.length > 3) return;
+    setRivals(e.target.value);
+    localStorage.setItem('rivals', e.target.value);
+  };
+
+  useEffect(() => {
+    window.addEventListener('storage', () => {
+      window.location.reload();
+    });
+    return () => {
+      window.removeEventListener('storage', () => {
+        window.location.reload();
+      });
+    };
+  }, []);
+
+  return (
+    <div className='app'>
+      <div className='counter'>
+        <p className='wins' onClick={addWin} onContextMenu={removeWin}>
+          {wins}
+        </p>
+        <div className='mid'>
+          <img
+            className='champs'
+            onClick={() => {
+              reset();
+            }}
+            src='/futchamps-wlcounter/assets/img/champions-only.png'
+            alt=''
+          />
+        </div>
+        <p className='loses' onClick={addLose} onContextMenu={removeLose}>
+          {loses}
+        </p>
+      </div>
+
+      {/* Alt Counter with video background - Uncomment to use it */}
+      {/*
+       <div className='alt-counter' style={{fontFamily: "'Rifton Norm', cursive"}}>
+        <video autoPlay loop muted playsInline className='alt-counter-video'>
+          <source src='/futchamps-wlcounter/assets/img/your-video.mp4' type='video/mp4' />
+        </video>
+      </div>
+
+      {/* Rivals Counter - Uncomment to use it */}
+      {/*
+      <div className='rivals'>
+        <div className='rivals-counter'>
+          <input
+            onChange={changeRivals}
+            type='number'
+            value={rivals}
+            style={rivals.length > 3 ? { fontSize: '40px' } : {}}
+          />
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
